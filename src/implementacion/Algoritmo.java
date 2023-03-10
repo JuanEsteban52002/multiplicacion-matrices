4c49b82c03a8b311eaa4d5f990411a82374598b9
JUANESTEBAN <juane.grandar@uqvirtual.edu.co>
2023-03-10 00:26:39 -0500

    Estructura Algoritmos

---
+++ src/implementacion/Algoritmo.java
@@ -0,0 +1,113 @@
+package implementacion;
+
+
+public class Algoritmo {
+
+	
+	public Algoritmo() {
+		
+	}
+	
+	public void naivStandard(double A, double B, double Result, int N, int P, int M) {
+		
+		double aux;
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				aux = 0.0;
+				for (int k = 0; k < P; k++) {
+					
+				}
+			}
+		}
+	}
+	
+	//Metodo 2
+	public void naivOnArray(double A, double B, double Result, int N, int P, int M) {
+		
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				
+				for (int k = 0; k < P; k++) {
+					
+				}
+			}
+		}
+	}
+	
+	//Metodo 3
+	public void naivKahan() {
+		
+	}
+	
+	//Metodo 4
+	public void naivLoopUnrollingTwo() {
+		
+	}
+	
+	//Metodo 5
+	public void naivLoopUnrollingThree() {
+		
+	}
+	
+	//Metodo 6
+	public void naivLoopUnrollingFour() {
+		
+	}
+	
+	//Metodo 7
+	public void winogradOriginal() {
+		
+	}
+	
+	//Metodo 8
+	public void winogradScaled() {
+		
+	}
+	
+	//Metodo 9
+	public void strassenNaiv() {
+		
+	}
+	
+	//Metodo 10
+	public void strassenWinograd() {
+		
+	}
+	
+	//Metodo 11
+	public void sequentialBlockIII() {
+		
+	}
+	
+	//Metodo 12
+	public void parallelBlockIII() {
+		
+	}
+	
+	//Metodo 13
+	public void sequentialBlockIV() {
+		
+	}
+	
+	//Metodo 14
+	public void parallelBlockIV() {
+		
+	}
+	
+	//Metodo 15
+	public void sequentialBlockV() {
+		
+	}
+	
+	//Metodo 16
+	public void parallelBlockV() {
+		
+	}
+	
+	
+	
+	
+
+}
